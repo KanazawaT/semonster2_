7f6123fddb86e2e54bbeaf6ed31e24b1406bda04
KnzwaT <m1m23a08@oit.ac.jp>
2024-07-08 10:24:32 +0900

    calling monster

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -10,5 +10,7 @@ public class App {
 
   public static void main(String[] args) {
     System.out.println(new App().getGreeting());
+    Monster monster = new Monster();
+    System.out.println(monster.call());
   }
 }
--- app/src/main/java/org/example/Monster.java
+++ app/src/main/java/org/example/Monster.java
@@ -1,5 +1,17 @@
 package org.example;
 
 public class Monster {
-  
+  public String name;
+  public int rarity;
+  static String[] rareRank = { "common", "uncommon", "rare", "SR", "SSR", "UR" };
+
+  Monster() {
+    this.name = "nanashi";
+    this.rarity = 0;
+  }
+
+  public String call() {
+    return rareRank[this.rarity] + " : " + this.name;
+  }
+
 }
